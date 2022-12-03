b6ec50eeeaab3e5328f240642fbada9c49b1937c
Joseff361 <Joseff361@outlook.com>
2022-12-03 17:33:24 -0500

    Intializing project & adding some configurations about navigation

---
+++ Section 8: Time To Practice - The Expense Tracker App/App.js
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+import { Fragment } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import RecentExpenses from './screens/RecentExpenses';
+import ManageExpense from './screens/ManageExpense';
+import AllExpenses from './screens/AllExpenses';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} />
+      <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+    </BottomTabs.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <Fragment>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+          <Stack.Screen name="MangeExpenses" component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Fragment>
+  );
+}
