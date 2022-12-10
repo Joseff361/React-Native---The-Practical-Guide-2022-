78d10cf464e4b5d478758457ce3f12f84bd37cd6
Joseff361 <Joseff361@outlook.com>
2022-12-09 22:23:39 -0500

    Initializing new section

---
+++ Section 9: Handling User Input/screens/RecentExpenses.js
@@ -0,0 +1,26 @@
+import { useContext } from 'react';
+import ExpensesOutput from '../components/Expenses/ExpensesOutput';
+import { ExpensesContext } from '../store/expenses-context';
+import { getDateMinusDays } from '../utils/date';
+
+function RecentExpenses() {
+  const expensesCtx = useContext(ExpensesContext);
+
+  // Expenses that occured in the last 7 days seen from today
+  const recentExpenses = expensesCtx.expenses.filter(expense => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+    console.log(expense.date, date7DaysAgo)
+    return expense.date > date7DaysAgo && expense.date <= today;
+  });
+
+  return (
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="Las 7 days"
+      fallbackText="No expenses registered for the last 7 days."
+    />
+  );
+}
+
+export default RecentExpenses;
